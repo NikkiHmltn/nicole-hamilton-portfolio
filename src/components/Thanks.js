f518b9ea1a13094bfda2617a898d54216a143995
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-07 17:20:42 -0800

    adding a thankyou after submission

---
+++ src/components/Thanks.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import './css/Thanks.css'
+
+export default class Thanks extends Component {
+    render(){
+        return(
+            <div className="thanks">
+                <h5>Thank you for your message! I look forward to hearing from you.🤩 </h5>
+            </div>
+        )
+        
+    }
+}
