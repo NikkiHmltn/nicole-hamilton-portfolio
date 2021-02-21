f5c81e89ed91a8311639b0eabcd060d51b3dfc62
Nicole Hamilton <Nicole@NicolesLaptop.local>
2021-02-20 17:37:22 -0800

    made the navbar a bit more flexible

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,7 @@ import Testimonials from './Testimonials'
 import Skills from './Skills'
 import NavBar from './NavBar'
 import './css/AboutMe.css'
-import me from './imgs/selfie.jpg'
+import me from './imgs/selfie.jpeg'
 
 export default class AboutMe extends Component {
     render(){
@@ -18,7 +18,11 @@ export default class AboutMe extends Component {
                         <div className="about-me" id="about-me">
                         <img src={me} width="150" height="150" style={{borderRadius: "50%"}}/>
                         <h1 className="about-title"> About Me </h1>
-                        <p >I’m a software engineer passionate about connecting humanity through empathy and technology; everyone has a different story to tell, and I want to provide that service to people through my coding. I strive to create organized and meaningful code through stubborn determination and creativity. With a background in Humanities and civil service, I provide critical thinking to projects and strive for team connection and communication to get the job done.</p>
+                        <p >I’m a software engineer passionate about connecting humanity through empathy and technology; everyone has a different story to tell, and I want to provide that service to people through my coding. I strive to create organized and meaningful code through stubborn determination and creativity. With a background in Humanities and civil service, I provide critical thinking to projects and strive for team connection and communication to get the job done.
+                        </p>
+                        <p>
+                            I enjoy working with collaborative teams to develop meaningful and fun applications for people to use. While I am a proficient full-stack developer, my expertise is utilizing a PERN stack (API services, user-friendly React front-end). Hard engineering problems are always a fun challenge to tackle on my own, but I really enjoy and thrive working with a team that strives for communication and meaningful applications to benefit society as a whole. I'm looking for a full-stack engineering role where I can take on team collaboration, and gain experience with team leadership and project management. 
+                        </p>
                     </div>
                     </Element>
                     <div className="testimonial-section">
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -5,15 +5,29 @@ export default class Testimonials extends Component {
     render(){
         return(
             <div className="testimonials">
-
-                <div className="test-header"> 
+                <div className="testimonial">
+                    <div className="test-header"> 
                     <h4> Simone Schneeberg</h4> <h6><em>(Full-Stack Engineer)</em></h6>
+                    </div>
+                    <div className="test-words">
+                        <p><em>
+                        "<strong>Nicole is an excellent team member.</strong> She is always reliable and on schedule.  She takes initiative and knows how to take suggestion and continue past it to return with something even more improved.  Anyone would be happy to have Nicole on their team."</em>
+                        </p>
+                    </div>
                 </div>
-                <div className="test-words">
-                    <p><em>
-                    "<strong>Nicole is an excellent team member.</strong> She is always reliable and on schedule.  She takes initiative and knows how to take suggestion and continue past it to return with something even more improved.  Anyone would be happy to have Nicole on their team."</em>
-                    </p>
+                <div className="testimonial">
+                    <div className="test-header">
+                        <h4>Daniel Alexander</h4> <h6><em>(Business Owner)</em></h6>
+                    </div>
+                    <div className="test-words">
+                        <p>"
+                            <em>
+                                Working with Nicole was a pleasure. <strong>Her attentive nature and rapid response to needs as they arose was impeccable.</strong> She added her own flair into the web page to not only make it come to life, but to also exceed all expectations and requirements. I would not only recommend her services, but would gladly employ her services for future projects.
+                            </em>"
+                        </p>
+                    </div>
                 </div>
+                
             </div>
         )
         
